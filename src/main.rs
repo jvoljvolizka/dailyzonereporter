9b304c39d8c1219a71752dc57eaf8a1633c4964b
jvol jvolizka <jvoljvolizka@gmail.com>
2020-12-07 00:55:57 +0300

    config struct and json deser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+//use std::fs::File;
+//use std::io;
+//use std::io::Read;
+//use std::process;
+use serde::{Deserialize, Serialize};
+//use serde_json::Result;
+use serde_derive::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    tlds: Vec<String> //The tld vector 
 }
 
+fn main() {
+    println!("i <3 golang");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        //probably there's a better way to do this.
+        panic!("give me a filename");
+    }
+    let config_file = &args[1];
+
+    let con = parse_config(config_file);
+    
+    
+}
+
+
+
+
+fn parse_config(filename: &str)  -> Config {
+    // i have no idea how to deal with two different error types
+    let contents = fs::read_to_string(filename).expect("File read err");
+
+    let con: Config = serde_json::from_str(&contents).expect("Json deser err");
+
+    for line in &con.tlds {
+        println!("{}", line)
+    }
+   
+   return con
+
+}
