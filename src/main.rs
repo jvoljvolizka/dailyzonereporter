8e4bdb5ac1c8b46aff420fb225806c794cae854c
jvol jvolizka <jvoljvolizka@gmail.com>
2020-12-07 23:37:58 +0300

    check old zonefiles

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::env;
-use std::fs;
+use std::fs::read_dir;
+use std::fs::read_to_string;
+use serde_json::from_str;
 //use std::fs::File;
 //use std::io;
 //use std::io::Read;
 //use std::process;
-use serde::{Deserialize, Serialize};
+//use serde::Deserialize;
 //use serde_json::Result;
 use serde_derive::{Serialize, Deserialize};
 
+
 #[derive(Serialize, Deserialize)]
 struct Config {
-    tlds: Vec<String> //The tld vector 
+    tlds: Vec<String> ,//The tld vector 
+    zonefile_dir : String
 }
 
 fn main() {
@@ -23,23 +27,40 @@ fn main() {
     let config_file = &args[1];
 
     let con = parse_config(config_file);
+    check_old_zonefiles(&con);
     
     
 }
 
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+fn check_old_zonefiles(con: &Config) {
+    // read the zonefile dir 
+    let paths = read_dir(&con.zonefile_dir).expect("Directory check err");
+    let mut files: Vec<String> = Vec::new();
 
+    for path in paths {// get every ok filename to a vector
+        if let Ok(path) = path {
+            let file = format!("{:?}" , path.file_name());
+            files.push(file);
+        }
+    }
+    for tld in &con.tlds { // check if every .old archive is in the zonefile dir 
+        //if you can't beat them join them
+        let check = format!("\"{}.txt.gz.old\"", tld); 
 
+        if !files.contains(&check){
+            panic!("Could not find {} file" , &check);
+        }
+    }
+    
+}
 
+//Parse config.json
 fn parse_config(filename: &str)  -> Config {
     // i have no idea how to deal with two different error types
-    let contents = fs::read_to_string(filename).expect("File read err");
-
-    let con: Config = serde_json::from_str(&contents).expect("Json deser err");
-
-    for line in &con.tlds {
-        println!("{}", line)
-    }
-   
-   return con
-
+    let contents = read_to_string(filename).expect("File read err");
+    let con: Config = from_str(&contents).expect("Json deser err");
+    return con
 }
